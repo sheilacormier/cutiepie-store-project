cd74375e9bf3eda5f56b6739581f0251d5bb2b8c
sheilacormier <sheila_cormier@hotmail.com>
2021-05-06 19:24:22 +0000

    completed footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,6 +5,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 import "../../styles/footer.scss";
 
 export const Footer = () => (
@@ -12,14 +13,34 @@ export const Footer = () => (
 		<Container>
 			<Row>
 				<Col xs={12} md={8}>
-					about us
+					<ul className="footerlinks">
+						<li>
+							<Link href="/">about us</Link>
+						</li>
+						<li>
+							<Link href="/">contact us</Link>
+						</li>
+						<li>
+							<Link href="/">terms and conditions</Link>
+						</li>
+						<li>
+							<Link href="/">privacy policy</Link>
+						</li>
+					</ul>
 				</Col>
 				<Col xs={6} md={4}>
-					image here
+					<img
+						src="https://res.cloudinary.com/scormier/image/upload/v1620161770/cutie-pie/footer-baby_yvxvyy.png"
+						alt="baby in red"
+						width="268"
+						height="184"
+					/>
 				</Col>
 			</Row>
 			<Row>
-				<Col>cuite pie. All rights reserved 2021</Col>
+				<Col>
+					<span className="copyright">&copy; cuite pie. All rights reserved 2021</span>
+				</Col>
 			</Row>
 		</Container>
 	</footer>
