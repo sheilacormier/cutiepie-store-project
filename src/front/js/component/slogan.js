24fb587819de82e6d0316695e9f81fcd2ea4d189
sheilacormier <sheila_cormier@hotmail.com>
2021-05-07 11:57:02 +0000

    commented out card shadow and updated slogan

--- src/front/js/component/slogan.js
+++ src/front/js/component/slogan.js
@@ -4,5 +4,14 @@ import "../../styles/slogan.scss";
 export const Slogan = () => (
 	<div className="mt-auto py-3 text-center">
 		<p className="slogan">NOTHING BUT THE BEST FOR YOUR BABY</p>
+		<p className="slogan">
+			CURATED WITH{" "}
+			<img
+				src="https://res.cloudinary.com/scormier/image/upload/v1620161770/cutie-pie/heart-large_trfvou.png"
+				alt="heart"
+				width="37"
+				height="34"
+			/>
+		</p>
 	</div>
 );
