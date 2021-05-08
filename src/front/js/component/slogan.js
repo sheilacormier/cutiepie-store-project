2e242260d51b954054006847cf53eeb1d2b32e74
naglenda31 <naglenda31@gmail.com>
2021-05-08 00:44:18 +0000

    updated slogan margin

--- src/front/js/component/slogan.js
+++ src/front/js/component/slogan.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import "../../styles/slogan.scss";
 
 export const Slogan = () => (
-	<div className="mt-auto py-3 text-center">
-		<p className="slogan">NOTHING BUT THE BEST FOR YOUR BABY</p>
-		<p className="slogan">
+	<div className="mt-auto pt-3 p-2 text-center">
+		<p className="slogan mb-1">NOTHING BUT THE BEST FOR YOUR BABY</p>
+		<p className="slogan mb-2">
 			CURATED WITH{" "}
 			<img
 				src="https://res.cloudinary.com/scormier/image/upload/v1620161770/cutie-pie/heart-large_trfvou.png"
