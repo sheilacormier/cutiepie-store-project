37ef3917b1325edef7eae7cde2cd8ea35ce4ee13
sheilacormier <sheila_cormier@hotmail.com>
2021-05-17 23:57:35 +0000

    working on fetches

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,176 +1,55 @@
 const getState = ({ getStore, getActions, setStore }) => {
+    const base_url = "https://3001-ivory-dingo-evk7hwf6.ws-us04.gitpod.io/";
 	return {
 		store: {
-			wishlist: [
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				}
-			],
-			shopCollection: [
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url:
-						"https://www.amazon.com/Ralph-Lauren-Cotton-Polo-Orange/dp/B07GV6XZVD/ref=sr_1_31?dchild=1&keywords=ralph+lauren+baby+clothes&qid=1620950637&sr=8-31"
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url:
-						"https://www.amazon.com/Polo-Ralph-Lauren-Two-Tone-Clubhouse/dp/B0814XSPMS/ref=sr_1_51?dchild=1&keywords=ralph+lauren+baby+clothes&qid=1620950637&sr=8-51"
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url:
-						"https://www.amazon.com/Lauren-Cotton-Interlock-Coverall-newborn/dp/B01B3LXGYQ/ref=sr_1_1?dchild=1&keywords=ralph+lauren+baby+clothes&qid=1620950637&sr=8-1"
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url:
-						"https://www.amazon.com/Ralph-Lauren-Shortall-Romper-Months/dp/B077SXNB8H/ref=sr_1_2?dchild=1&keywords=ralph+lauren+baby+clothes&qid=1620950637&sr=8-2"
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				},
-				{
-					h1: "Leopard Onesie",
-					img: "https://via.placeholder.com/250x220?text=Placeholder",
-					url: ""
-				}
-			],
-
-			homeCards: [
-				{
-					id: "1",
-					price: "$35",
-					img: "https://res.cloudinary.com/scormier/image/upload/v1620334490/cutie-pie/outfit1_l8oaq2.png",
-					url: ""
-				},
-				{
-					id: "2",
-					price: "$45",
-					img: "https://res.cloudinary.com/scormier/image/upload/v1620334490/cutie-pie/outfit2_hdw0oc.png",
-					url: ""
-				},
-				{
-					id: "3",
-					price: "$25",
-					img: "https://res.cloudinary.com/scormier/image/upload/v1620334490/cutie-pie/outfit3_wco9ga.png",
-					url: ""
-				},
-				{
-					id: "4",
-					price: "$55",
-					img:
-						"https://res.cloudinary.com/scormier/image/upload/v1620485867/cutie-pie/sample-details1_sm_my7qxl.png",
-					url:
-						"https://www.amazon.com/Polo-Ralph-Lauren-Interlock-Shortall/dp/B08FYCBKJW/ref=sr_1_7?dchild=1&keywords=ralph+lauren+baby+clothes&qid=1620950637&sr=8-7"
-				}
-			],
-
-			productDetails: [
-				{
-					id: "1",
-					brand: "Polo Ralph Lauren",
-					title: "Baby Boys Cotton Interlock Polo Shortall",
-					price: "$35",
-					color: "blue",
-					size: "0-3 months",
-					img: "https://res.cloudinary.com/scormier/image/upload/v1620334490/cutie-pie/outfit1_l8oaq2.png",
-					description:
-						"Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et",
-					url: ""
-				},
-				{
-					id: "2",
-					brand: "Polo Ralph Lauren",
-					title: "Baby Boys Cotton Interlock Polo Shortall",
-					price: "$45",
-					color: "blue",
-					size: "0-3 months",
-					img: "https://res.cloudinary.com/scormier/image/upload/v1620334490/cutie-pie/outfit2_hdw0oc.png",
-					description:
-						"Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et",
-					url: ""
-				},
-				{
-					id: "3",
-					brand: "Polo Ralph Lauren",
-					title: "Baby Boys Cotton Interlock Polo Shortall",
-					price: "$25",
-					color: "blue",
-					size: "0-3 months",
-					img: "https://res.cloudinary.com/scormier/image/upload/v1620334490/cutie-pie/outfit3_wco9ga.png",
-					description:
-						"Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et",
-					url: ""
-				},
-				{
-					id: "4",
-					brand: "Polo Ralph Lauren",
-					title: "Baby Boys Cotton Interlock Polo Shortall",
-					price: "$55",
-					color: "blue",
-					size: "0-3 months",
-					img:
-						"https://res.cloudinary.com/scormier/image/upload/v1620485867/cutie-pie/sample-details1_sm_my7qxl.png",
-					description:
-						"Suspendisse quos? Tempus cras iure temporibus? Eu laudantium cubilia sem sem! Repudiandae et",
-					url: ""
-				}
-			]
+            wishlist: [],
+            product: [],
+			shopCollection: [],
+			homeCards: [],
+			productDetails: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+            // Use getActions to call a function within a fuction
+            initialize: () => {
+				fetch(`${base_url}/user`)
+					.then(res => res.json())
+					.then(data => setStore({ user: data.user }));
+
+				fetch(`${base_url}/product`)
+					.then(res => res.json())
+					.then(data => setStore({ product: data.product }));
+            },
+            
+            addWishlist: data => {
+				//get the store
+				const store = getStore();
+				//and pushes data
+				store.wishlist.push(data);
+				//reset the global store
+				setStore(store);
 			},
+
+			removeWishilist: index => {
+				//get the store
+				const store = getStore();
+				//and filter data
+				let wishlist = store.wishlist.filter((item, i) => i !== index);
+				//reset the global store
+				setStore({ wishlist: wishlist });
+			},
+
+			countWishlist: () => {
+				//get the store
+				const store = getStore();
+				//and count data
+				const length = store.wishlist.length;
+				//return results
+				return length;
+			},
+
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
 
 			getMessage: () => {
 				// fetching data from the backend
