d08bf89e7b5a04b3fe46fe33034b114bc35e67df
naglenda31 <naglenda31@gmail.com>
2021-05-19 01:28:52 +0000

    updated shop collection cards background

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -91,7 +91,12 @@ export const Profile = () => {
 								<Nav.Link className="py-1 px-2 pr-md-4" as={Link} to="/wishlist">
 									my wishlist
 								</Nav.Link>
-								<Nav.Link className="py-1 px-2 pl-md-4 logout" as={Link} to="/logout">
+
+								<Nav.Link
+									className="py-1 px-2 pl-md-4 logout"
+									as={Link}
+									to="/"
+									onClick={e => actions.signout()}>
 									logout
 								</Nav.Link>
 							</Nav>
