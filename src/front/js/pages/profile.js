6e51e1319e10c33559a1e18d971fec1317fe29d6
naglenda31 <naglenda31@gmail.com>
2021-05-25 00:22:04 +0000

    updated hero image in profile page

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -93,82 +93,87 @@ export const Profile = () => {
 		reader.readAsDataURL(file);
 	};
 	return (
-		<Container className="my-1">
-			{typeof store.user.id !== "undefined" && (
-				<Row className="mx-auto align-items-center justify-content-center mt-2">
-					<Col xs={12} md={6} className="p-0">
-						<div className="d-flex flex-column profile-container profile-container-left p-2">
-							<h3 className="text-center">Your User Profile</h3>
+		<Container fluid>
+			<Container className="my-1">
+				{typeof store.user.id !== "undefined" && (
+					<Row className="mx-auto align-items-center justify-content-center mt-2">
+						<Col xs={12} md={6} className="p-0">
+							<div className="d-flex flex-column profile-container profile-container-left p-2">
+								<h3 className="text-center">Your User Profile</h3>
 
-							<img
-								src={
-									src !== ""
-										? src
-										: "https://res.cloudinary.com/scormier/image/upload/v1620690465/cutie-pie/1452484590-sweet-baby-boy-image-screenshot_wzzhx5.jpg"
-								}
-								className="align-self-center m-2 rounded-circle profile-image"
-								alt="User-Profile-Image"
-							/>
-
-							<Form
-								className="d-flex flex-column flex-md-row align-items-center justify-content-center"
-								onSubmit={uploadImage}>
-								<Form.File.Input
-									className="w-50 form-file mt-1"
-									onChange={e => {
-										changeImage(e.target.files[0]);
-										setImage(e.target.files[0]);
-									}}
+								<img
+									src={
+										src !== ""
+											? src
+											: "https://res.cloudinary.com/scormier/image/upload/v1620690465/cutie-pie/1452484590-sweet-baby-boy-image-screenshot_wzzhx5.jpg"
+									}
+									className="align-self-center m-2 rounded-circle profile-image"
+									alt="User-Profile-Image"
 								/>
-								<Button type="submit" className="mt-2" bsPrefix="btn-upload-photo" variant="warning">
-									Upload photo
-								</Button>
-							</Form>
 
-							<Nav className="justify-content-between justify-content-md-center">
-								<Nav.Link className="py-1 px-2 pr-md-4" as={Link} to="/wishlist">
-									my wishlist
-								</Nav.Link>
+								<Form
+									className="d-flex flex-column flex-md-row align-items-center justify-content-center"
+									onSubmit={uploadImage}>
+									<Form.File.Input
+										className="w-50 form-file mt-1"
+										onChange={e => {
+											changeImage(e.target.files[0]);
+											setImage(e.target.files[0]);
+										}}
+									/>
+									<Button
+										type="submit"
+										className="mt-2"
+										bsPrefix="btn-upload-photo"
+										variant="warning">
+										Upload photo
+									</Button>
+								</Form>
 
-								<Nav.Link
-									className="py-1 px-2 pl-md-4 logout"
-									as={Link}
-									to="/"
-									onClick={e => actions.signout()}>
-									logout
-								</Nav.Link>
-							</Nav>
-						</div>
-					</Col>
-					<Col xs={12} md={6} className="p-0">
-						<div className="profile-container py-3 px-2 d-flex flex-column">
-							<Form noValidate validated={emailValid} onSubmit={handleEmailSubmit}>
-								<h3 className="text-center">Login Information</h3>
-								<Form.Group controlId="formBasicEmail" className=" mx-2 mt-2 mb-0">
-									<div className="d-flex flex-column">
-										<Form.Control
-											type="email"
-											value={email}
-											placeholder="Email"
-											required
-											onChange={e => setEmail(e.target.value)}
-											ref={emailRef}
-										/>
-										<Form.Control.Feedback as="div" type="invalid" className="w-50">
-											{emailRef && emailRef.current && emailRef.current.validationMessage}
-										</Form.Control.Feedback>
-										<Button
-											type="submit"
-											className="align-self-end"
-											bsPrefix="btn-update-email"
-											variant="warning">
-											Update Email
-										</Button>
-									</div>
-								</Form.Group>
-							</Form>
+								<Nav className="justify-content-between justify-content-md-center">
+									<Nav.Link className="py-1 px-2 pr-md-4" as={Link} to="/wishlist">
+										my wishlist
+									</Nav.Link>
 
-							{/* <div className="d-flex flex-column">
+									<Nav.Link
+										className="py-1 px-2 pl-md-4 logout"
+										as={Link}
+										to="/"
+										onClick={e => actions.signout()}>
+										logout
+									</Nav.Link>
+								</Nav>
+							</div>
+						</Col>
+						<Col xs={12} md={6} className="p-0">
+							<div className="profile-container py-3 px-2 d-flex flex-column">
+								<Form noValidate validated={emailValid} onSubmit={handleEmailSubmit}>
+									<h3 className="text-center">Login Information</h3>
+									<Form.Group controlId="formBasicEmail" className=" mx-2 mt-2 mb-0">
+										<div className="d-flex flex-column">
+											<Form.Control
+												type="email"
+												value={email}
+												placeholder="Email"
+												required
+												onChange={e => setEmail(e.target.value)}
+												ref={emailRef}
+											/>
+											<Form.Control.Feedback as="div" type="invalid" className="w-50">
+												{emailRef && emailRef.current && emailRef.current.validationMessage}
+											</Form.Control.Feedback>
+											<Button
+												type="submit"
+												className="align-self-end"
+												bsPrefix="btn-update-email"
+												variant="warning">
+												Update Email
+											</Button>
+										</div>
+									</Form.Group>
+								</Form>
+
+								{/* <div className="d-flex flex-column">
 									<Form.Control type="password" placeholder="Current Password" required />
 									<Button
 										type="submit"
@@ -178,39 +183,39 @@ export const Profile = () => {
 										Update Password
 									</Button>
 								</div> */}
-							<Form noValidate validated={passValid} onSubmit={handlePassSubmit}>
-								<Form.Group controlId="formPassword" className=" mx-2 pt-1 mb-0">
-									<div className="d-flex flex-column">
-										<Form.Control
-											type="password"
-											value={password}
-											placeholder="New Password"
-											required
-											onChange={e => setPassword(e.target.value)}
-											ref={passRef}
-										/>
+								<Form noValidate validated={passValid} onSubmit={handlePassSubmit}>
+									<Form.Group controlId="formPassword" className=" mx-2 pt-1 mb-0">
+										<div className="d-flex flex-column">
+											<Form.Control
+												type="password"
+												value={password}
+												placeholder="New Password"
+												required
+												onChange={e => setPassword(e.target.value)}
+												ref={passRef}
+											/>
 
-										<Form.Control.Feedback type="invalid">
-											{passRef && passRef.current && passRef.current.validationMessage}
-										</Form.Control.Feedback>
+											<Form.Control.Feedback type="invalid">
+												{passRef && passRef.current && passRef.current.validationMessage}
+											</Form.Control.Feedback>
 
-										<Button
-											type="submit"
-											className="align-self-end"
-											bsPrefix="btn-update-password"
-											variant="warning">
-											Update Password
-										</Button>
-									</div>
-								</Form.Group>
-							</Form>
-						</div>
-					</Col>
-				</Row>
-			)}
-
+											<Button
+												type="submit"
+												className="align-self-end"
+												bsPrefix="btn-update-password"
+												variant="warning">
+												Update Password
+											</Button>
+										</div>
+									</Form.Group>
+								</Form>
+							</div>
+						</Col>
+					</Row>
+				)}
+			</Container>
 			<Row className="justify-content-center align-items-center my-4">
-				<Col>
+				<Col className="p-0">
 					<div className="hero-image-profile" />
 				</Col>
 			</Row>
