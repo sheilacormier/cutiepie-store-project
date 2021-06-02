674d196832980df07a8d6b09948d3de0a46bf1f4
naglenda31 <naglenda31@gmail.com>
2021-06-02 01:40:18 +0000

    updated user login route alert message

--- src/api/routes.py
+++ src/api/routes.py
@@ -39,7 +39,7 @@ def login():
     user = User.query.filter_by(email=email).first()
     if user is None:
         return jsonify({
-            "msg": "You are not a registered user, please create an account"
+            "msg": "You are not a registered user, please create an account."
         }), 401
     
     if password != user.password:
